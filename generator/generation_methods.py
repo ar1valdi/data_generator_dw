bf9d1f421a2624222925e2ed16f44cf27f4c57a8
ar1valdi <jkaczerski@gmail.com>
2024-10-24 15:34:28 +0200

    created names generation and all models

--- generator/generation_methods.py
+++ generator/generation_methods.py
@@ -13,9 +13,14 @@ class Sex(Enum):
     FEMALE = 2
 
 
+TITLES = [None, "Licencjat", "Inżynier", "Magister", "Magister Inżynier", "Doktor", "Doktor Habilitowany", "Profesor"]
+TITLES_WORKER_PROB = [0, 1, 5, 1, 15, 50, 19, 9]
+TITLES_STUDENT_PROB = [60, 28, 2, 2, 8, 0, 0, 0]
+
+
 def generate_names_and_surname(sex: Sex):
-    names_path = MALE_NAMES_FILEPATH if sex is sex.MALE else FEMALE_NAMES_FILEPATH
-    surnames_path = MALE_SURNAMES_FILEPATH if sex is sex.MALE else FEMALE_SURNAMES_FILEPATH
+    names_path = MALE_NAMES_FILEPATH if sex is Sex.MALE else FEMALE_NAMES_FILEPATH
+    surnames_path = MALE_SURNAMES_FILEPATH if sex is Sex.MALE else FEMALE_SURNAMES_FILEPATH
 
     with (open(names_path, 'r', encoding='utf-8') as names_file,
           open(surnames_path, 'r', encoding='utf-8') as surnames_file):
@@ -26,4 +31,9 @@ def generate_names_and_surname(sex: Sex):
     nazwisko = random.choice(all_surnames)
 
     return imiona[0].strip(), imiona[1].strip(), nazwisko.strip()
+
+
+def generate_scientific_title(for_student: bool):
+    prob = TITLES_STUDENT_PROB if for_student else TITLES_WORKER_PROB
+    return random.choices(TITLES, weights=prob, k=1)[0]
 
--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from generator.generation_methods import *
 
 for i in range(10):
+    student = random.choice([True, False])
     i1, i2, n = generate_names_and_surname(Sex.MALE)
-    print(i1, i2, n)
+    title = generate_scientific_title(student)
+    print('student?: ', student, ' - ', i1, i2, n, ' [', title, ']')
 
 for i in range(10):
+    student = random.choice([True, False])
     i1, i2, n = generate_names_and_surname(Sex.FEMALE)
-    print(i1, i2, n)
+    title = generate_scientific_title(student)
+    print('student?: ', student, ' - ', i1, i2, n, ' [', title, ']')
