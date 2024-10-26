65f947a45fdbbd2f86490f90ff5ba45e72c47a6f
Ebederowo <ebederowo@outlook.com>
2024-10-26 18:07:39 +0200

    course and study name generation

--- generator/generation_methods.py
+++ generator/generation_methods.py
@@ -1,12 +1,162 @@
 from enum import Enum
 import random
 from faker import Faker
+import datetime
+
+from models.sql_models import *
 
 MALE_NAMES_FILEPATH = 'names_surnames_data/men_names.txt'
 MALE_SURNAMES_FILEPATH = 'names_surnames_data/men_surnames.txt'
 FEMALE_NAMES_FILEPATH = 'names_surnames_data/women_names.txt'
 FEMALE_SURNAMES_FILEPATH = 'names_surnames_data/women_surnames.txt'
 
+COURSE_MASC_SING_NOUNS_FILEPATH = 'courses_data/nouns/masc_sing.txt'
+COURSE_FEM_SING_NOUNS_FILEPATH = 'courses_data/nouns/fem_sing.txt'
+COURSE_NEUT_SING_NOUNS_FILEPATH = 'courses_data/nouns/neut_sing.txt'
+COURSE_MASC_PLUR_NOUNS_FILEPATH = 'courses_data/nouns/masc_plur.txt'
+COURSE_NONMASC_PLUR_NOUNS_FILEPATH = 'courses_data/nouns/nonmasc_plur.txt'
+
+COURSE_MASC_SING_ADJECTIVES_FILEPATH = 'courses_data/adjectives/masc_sing.txt'
+COURSE_FEM_SING_ADJECTIVES_FILEPATH = 'courses_data/adjectives/fem_sing.txt'
+COURSE_NEUT_SING_ADJECTIVES_FILEPATH = 'courses_data/adjectives/neut_sing.txt'
+COURSE_MASC_PLUR_ADJECTIVES_FILEPATH = 'courses_data/adjectives/masc_plur.txt'
+COURSE_NONMASC_PLUR_ADJECTIVES_FILEPATH = 'courses_data/adjectives/nonmasc_plur.txt'
+
+STUDY_MASC_SING_NOUNS_FILEPATH = 'studies_data/nouns/masc_sing.txt'
+STUDY_FEM_SING_NOUNS_FILEPATH = 'studies_data/nouns/fem_sing.txt'
+STUDY_NEUT_SING_NOUNS_FILEPATH = 'studies_data/nouns/neut_sing.txt'
+STUDY_MASC_PLUR_NOUNS_FILEPATH = 'studies_data/nouns/masc_plur.txt'
+STUDY_NONMASC_PLUR_NOUNS_FILEPATH = 'studies_data/nouns/nonmasc_plur.txt'
+
+STUDY_MASC_SING_ADJECTIVES_FILEPATH = 'studies_data/adjectives/masc_sing.txt'
+STUDY_FEM_SING_ADJECTIVES_FILEPATH = 'studies_data/adjectives/fem_sing.txt'
+STUDY_NEUT_SING_ADJECTIVES_FILEPATH = 'studies_data/adjectives/neut_sing.txt'
+STUDY_MASC_PLUR_ADJECTIVES_FILEPATH = 'studies_data/adjectives/masc_plur.txt'
+STUDY_NONMASC_PLUR_ADJECTIVES_FILEPATH = 'studies_data/adjectives/nonmasc_plur.txt'
+
+class Lexicon():
+    def __init__(self, filepaths):
+        with (open(filepaths[0], 'r', encoding='utf-8') as masc_sing_nouns_file,
+              open(filepaths[1], 'r', encoding='utf-8') as fem_sing_nouns_file,
+              open(filepaths[2], 'r', encoding='utf-8') as neut_sing_nouns_file,
+              open(filepaths[3], 'r', encoding='utf-8') as masc_plur_nouns_file,
+              open(filepaths[4], 'r', encoding='utf-8') as nonmasc_plur_nouns_file,
+              open(filepaths[5], 'r', encoding='utf-8') as masc_sing_adjectives_file,
+              open(filepaths[6], 'r', encoding='utf-8') as fem_sing_adjectives_file,
+              open(filepaths[7], 'r', encoding='utf-8') as neut_sing_adjectives_file,
+              open(filepaths[8], 'r', encoding='utf-8') as masc_plur_adjectives_file,
+              open(filepaths[9], 'r', encoding='utf-8') as nonmasc_plur_adjectives_file):
+            masc_sing_nouns = masc_sing_nouns_file.readlines()
+            fem_sing_nouns = fem_sing_nouns_file.readlines()
+            neut_sing_nouns = neut_sing_nouns_file.readlines()
+            masc_plur_nouns = masc_plur_nouns_file.readlines()
+            nonmasc_plur_nouns = nonmasc_plur_nouns_file.readlines()
+
+            masc_sing_adjectives = masc_sing_adjectives_file.readlines()
+            fem_sing_adjectives = fem_sing_adjectives_file.readlines()
+            neut_sing_adjectives = neut_sing_adjectives_file.readlines()
+            masc_plur_adjectives = masc_plur_adjectives_file.readlines()
+            nonmasc_plur_adjectives = nonmasc_plur_adjectives_file.readlines()
+
+            self.noun_gender_weights = [len(nouns) for nouns in [masc_sing_nouns, fem_sing_nouns, neut_sing_nouns, masc_plur_nouns, nonmasc_plur_nouns]]
+            
+            self.masc_sing_nouns = [line.strip().split(" ") for line in masc_sing_nouns]
+            self.fem_sing_nouns = [line.strip().split(" ") for line in fem_sing_nouns]
+            self.neut_sing_nouns = [line.strip().split(" ") for line in neut_sing_nouns]
+            self.masc_plur_nouns = [line.strip().split(" ") for line in masc_plur_nouns]
+            self.nonmasc_plur_nouns = [line.strip().split(" ") for line in nonmasc_plur_nouns]
+
+            self.masc_sing_adjectives = [line.strip().split(" ") for line in masc_sing_adjectives]
+            self.fem_sing_adjectives = [line.strip().split(" ") for line in fem_sing_adjectives]
+            self.neut_sing_adjectives = [line.strip().split(" ") for line in neut_sing_adjectives]
+            self.masc_plur_adjectives = [line.strip().split(" ") for line in masc_plur_adjectives]
+            self.nonmasc_plur_adjectives = [line.strip().split(" ") for line in nonmasc_plur_adjectives]
+
+    def pickGender(self):
+        return random.choices([g for g in GrammaticalGender], weights=self.noun_gender_weights)[0]
+    
+    def generate_noun(self, case):
+        gender = self.pickGender()
+        match gender:
+            case GrammaticalGender.MASCULINE:
+                return random.choice(self.masc_sing_nouns)[case]
+            case GrammaticalGender.FEMININE:
+                return random.choice(self.fem_sing_nouns)[case]
+            case GrammaticalGender.NEUTER:
+                return random.choice(self.neut_sing_nouns)[case]
+            case GrammaticalGender.MASCULINEPERSONAL:
+                return random.choice(self.masc_plur_nouns)[case]
+            case GrammaticalGender.NONMASCULINEPERSONAL:
+                return random.choice(self.nonmasc_plur_nouns)[case]
+            case _:
+                return "_"
+
+    def generate_concord(self, case):
+        gender = self.pickGender()
+        match gender:
+            case GrammaticalGender.MASCULINE:
+                return random.choice(self.masc_sing_nouns)[case] + " " + random.choice(self.masc_sing_adjectives)[case]
+            case GrammaticalGender.FEMININE:
+                return random.choice(self.fem_sing_nouns)[case] + " " + random.choice(self.fem_sing_adjectives)[case]
+            case GrammaticalGender.NEUTER:
+                return random.choice(self.neut_sing_nouns)[case] + " " + random.choice(self.neut_sing_adjectives)[case]
+            case GrammaticalGender.MASCULINEPERSONAL:
+                return random.choice(self.masc_plur_nouns)[case] + " " + random.choice(self.masc_plur_adjectives)[case]
+            case GrammaticalGender.NONMASCULINEPERSONAL:
+                return random.choice(self.nonmasc_plur_nouns)[case] + " " + random.choice(self.nonmasc_plur_adjectives)[case]
+            case _:
+                return "_"
+
+    def generate_noun_nom(self):
+        return self.generate_noun(0)
+    
+    def generate_concord_nom(self):
+        return self.generate_concord(0)
+
+    def generate_noun_gen(self):
+        return self.generate_noun(1)
+
+    def generate_concord_gen(self):
+        return self.generate_concord(1)
+
+    def generate_noun_loc(self):
+        return self.generate_noun(2)
+
+    def generate_concord_loc(self):
+        return self.generate_concord(2)
+
+class CourseLexicon(Lexicon):
+    def __init__(self):
+        super().__init__([COURSE_MASC_SING_NOUNS_FILEPATH, 
+                          COURSE_FEM_SING_NOUNS_FILEPATH, 
+                          COURSE_NEUT_SING_NOUNS_FILEPATH, 
+                          COURSE_MASC_PLUR_NOUNS_FILEPATH, 
+                          COURSE_NONMASC_PLUR_NOUNS_FILEPATH, 
+                          COURSE_MASC_SING_ADJECTIVES_FILEPATH, 
+                          COURSE_FEM_SING_ADJECTIVES_FILEPATH, 
+                          COURSE_NEUT_SING_ADJECTIVES_FILEPATH, 
+                          COURSE_MASC_PLUR_ADJECTIVES_FILEPATH,
+                          COURSE_NONMASC_PLUR_ADJECTIVES_FILEPATH])
+
+class StudyLexicon(Lexicon):
+    def __init__(self):
+        super().__init__([STUDY_MASC_SING_NOUNS_FILEPATH, 
+                          STUDY_FEM_SING_NOUNS_FILEPATH, 
+                          STUDY_NEUT_SING_NOUNS_FILEPATH, 
+                          STUDY_MASC_PLUR_NOUNS_FILEPATH, 
+                          STUDY_NONMASC_PLUR_NOUNS_FILEPATH, 
+                          STUDY_MASC_SING_ADJECTIVES_FILEPATH, 
+                          STUDY_FEM_SING_ADJECTIVES_FILEPATH, 
+                          STUDY_NEUT_SING_ADJECTIVES_FILEPATH, 
+                          STUDY_MASC_PLUR_ADJECTIVES_FILEPATH,
+                          STUDY_NONMASC_PLUR_ADJECTIVES_FILEPATH])
+
+class GrammaticalGender(Enum):
+    MASCULINE = 1
+    FEMININE = 2
+    NEUTER = 3
+    MASCULINEPERSONAL = 4
+    NONMASCULINEPERSONAL = 5
 
 class Sex(Enum):
     MALE = 1
@@ -53,3 +203,53 @@ def generate_student(date_from, date_to):
         min_age, max_age = 23, 50
 
     data_urodzenia = fake.date_of_birth(minimum_age=min_age, maximum_age=max_age)
+
+# noun
+# concord = noun + adjective
+
+def generate_course_name(lexicon):
+    course_name_recipes = [
+        lambda lex : lex.generate_concord_nom(),
+        lambda lex : lex.generate_noun_nom() + " " + lex.generate_concord_gen(),
+        lambda lex : lex.generate_noun_nom() + " " + lex.generate_noun_gen() + " " + lex.generate_noun_gen(),
+        lambda lex : lex.generate_noun_nom() + " " + lex.generate_noun_gen(),
+        lambda lex : lex.generate_noun_nom() + " i " + lex.generate_noun_nom(),
+        lambda lex : lex.generate_noun_nom() + " w " + lex.generate_noun_loc(),
+        lambda lex : lex.generate_concord_nom() + " w " + lex.generate_noun_loc(),
+        lambda lex : lex.generate_noun_nom() + " w " + lex.generate_concord_loc(),
+        lambda lex : lex.generate_noun_nom() + " " + lex.generate_noun_gen() + " w " + lex.generate_noun_loc()
+    ]
+    recipe = random.choice(course_name_recipes)
+
+    return recipe(lexicon)
+
+def generate_course(lexicon, date_from, date_to):
+    fake = Faker()
+
+    name = generate_course_name(lexicon)
+    hours = random.randrange(10,121,5)
+    ects = random.randint(1,8)
+    id = None
+    creation_date = fake.date_between(date_from, date_to)
+    worker = None
+    assigned_study = None
+
+    return Kurs(name, hours, ects, id, creation_date, worker, assigned_study)
+
+def generate_study_name(lexicon):
+    study_name_recipes = [
+        lambda lex : lex.generate_concord_nom(),
+        lambda lex : lex.generate_noun_nom(),
+        lambda lex : lex.generate_noun_nom() + " i " + lex.generate_noun_nom(),
+        lambda lex : lex.generate_noun_nom() + ", " + lex.generate_noun_nom() + " i " + lex.generate_noun_nom()
+    ]
+    recipe = random.choice(study_name_recipes)
+
+    return recipe(lexicon)
+
+def generate_study(lexicon, year_from, year_to):
+    name = generate_study_name(lexicon)
+    year = random.randint(year_from, year_to)
+    
+    return Kierunek(name, year) 
+
--- main.py
+++ main.py
@@ -11,3 +11,13 @@ for i in range(10):
     i1, i2, n = generate_names_and_surname(Sex.FEMALE)
     title = generate_scientific_title(student)
     print('student?: ', student, ' - ', i1, i2, n, ' [', title, ']')
+
+cl = CourseLexicon()
+sl = StudyLexicon()
+for _ in range(20):
+    s = generate_study(sl, 2017, 2024)
+    print(s.nazwa, s.rok_rozpoczecia)
+
+for _ in range(20):
+    c = generate_course(cl, datetime.date(2017,1,1), datetime.date(2024,10,1))
+    print(c.nazwa, c.data_utworzenia, c.ilosc_godzin, c.liczba_ects)
